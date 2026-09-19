8fad24ba563f2260959fd5c683a99fc02bf5ca7d
DenisDK <unworkdk@gmail.com>
2026-09-19 20:34:40 +0300

    shopping card start template

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import { Counter } from "@/modules/counter";
+// import { Counter } from "@/modules/counter";
+import { ShoppingCart } from "@/modules/shoppingCart";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
         {/* justify-between */}
-        <Counter />
+        {/* <Counter /> */}
+        <ShoppingCart />
       </main>
     </div>
   );
---
+++ modules/shoppingCart/data/products.ts
@@ -0,0 +1,25 @@
+import { Product } from "../types/cart.types";
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: "Keyboard",
+    price: 80,
+    category: "Accessories",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 2,
+    name: "Mouse",
+    price: 50,
+    category: "Accessories",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 3,
+    name: "Monitor",
+    price: 300,
+    category: "Displays",
+    image: "https://picsum.photos/200/300",
+  },
+];
