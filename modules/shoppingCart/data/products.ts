b848693553c0d4dc1413057a66ec5d8b9eeca5f1
DenisDK <unworkdk@gmail.com>
2026-09-20 19:44:17 +0300

    test data arr

--- modules/shoppingCart/data/products.ts
+++ modules/shoppingCart/data/products.ts
@@ -4,6 +4,8 @@ export const products: Product[] = [
   {
     id: 1,
     name: "Keyboard",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, blanditiis voluptatem laudantium?",
     price: 80,
     category: "Accessories",
     image: "https://picsum.photos/200/300",
@@ -11,6 +13,8 @@ export const products: Product[] = [
   {
     id: 2,
     name: "Mouse",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, blanditiis voluptatem laudantium?",
     price: 50,
     category: "Accessories",
     image: "https://picsum.photos/200/300",
@@ -18,6 +22,8 @@ export const products: Product[] = [
   {
     id: 3,
     name: "Monitor",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, blanditiis voluptatem laudantium?",
     price: 300,
     category: "Displays",
     image: "https://picsum.photos/200/300",
