cb77fb73ad5602a89dbbcbe987e55f02e340d961
DenisDK <unworkdk@gmail.com>
2026-09-20 20:53:56 +0300

    minor fix

--- modules/shoppingCart/components/Cart.tsx
+++ modules/shoppingCart/components/Cart.tsx
@@ -8,14 +8,14 @@ import { FaDollarSign } from "react-icons/fa6";
 const Cart = () => {
   const cartItems = useCartStore((state) => state.cartItems);
   return (
-    <div>
-      <h2 className="">Cart</h2>
+    <div className="mt-5">
+      <h2 className="font-bold">Cart:</h2>
 
       {cartItems.map((item) => (
         <div key={item.id} className="">
-          <p>{item.name}</p>
+          <p>Name: {item.name}</p>
           <p>Quantity: {item.quantity}</p>
-          <p>{item.price}</p>
+          <p>Price: {item.price}</p>
           <p className="flex items-center">
             Price for ({item.name}): {item.price * item.quantity}{" "}
             <FaDollarSign />
