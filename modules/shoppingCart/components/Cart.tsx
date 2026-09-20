cf99038d899705b664ec1a49977ec59ed8f072fe
DenisDK <unworkdk@gmail.com>
2026-09-20 21:04:34 +0300

    add total cart price

--- modules/shoppingCart/components/Cart.tsx
+++ modules/shoppingCart/components/Cart.tsx
@@ -7,6 +7,12 @@ import { FaDollarSign } from "react-icons/fa6";
 
 const Cart = () => {
   const cartItems = useCartStore((state) => state.cartItems);
+
+  const totalPrice = cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0,
+  );
+
   return (
     <div className="mt-5">
       <h2 className="font-bold">Cart:</h2>
@@ -22,6 +28,9 @@ const Cart = () => {
           </p>
         </div>
       ))}
+      <div className="flex items-center mt-5 font-bold">
+        Total cart price: {totalPrice} <FaDollarSign />
+      </div>
     </div>
   );
 };
