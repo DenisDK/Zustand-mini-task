9a245ef1b5b9ad7bb89d6a92ca89cb127efdb83d
DenisDK <unworkdk@gmail.com>
2026-09-23 21:12:20 +0300

    wishlist

--- modules/wishlist/components/ProductList.tsx
+++ modules/wishlist/components/ProductList.tsx
@@ -7,41 +7,53 @@ import Image from "next/image";
 
 // Icons
 import { FaRegStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
 
 const ProductList = () => {
   const toggleFavorite = useWishlistStore((state) => state.toggleFavorite);
+  const favorites = useWishlistStore((state) => state.favorites);
 
   return (
     <div className="flex items-center gap-5">
-      {products.map((product) => (
-        <div className="max-w-52 border rounded-lg bg-white/5" key={product.id}>
-          <Image
-            className="w-full h-48 object-cover rounded-t-lg"
-            src={product.image}
-            alt={product.name}
-            width={1000}
-            height={1000}
-            loading="eager"
-          />
-          <div className="p-2">
-            <h3 className="font-bold">{product.name}</h3>
-            <p className="font-light italic text-sm mt-2">
-              {product.description}
-            </p>
-            <p className="text-sm mt-1">{product.category}</p>
+      {products.map((product) => {
+        const isFavorite = favorites.some(
+          (favorite) => favorite.id === product.id,
+        );
+
+        return (
+          <div
+            className="max-w-52 border rounded-lg bg-white/5"
+            key={product.id}
+          >
+            <Image
+              className="w-full h-48 object-cover rounded-t-lg"
+              src={product.image}
+              alt={product.name}
+              width={1000}
+              height={1000}
+              loading="eager"
+            />
+
+            <div className="p-2">
+              <h3 className="font-bold">{product.name}</h3>
+
+              <p className="font-light italic text-sm mt-2">
+                {product.description}
+              </p>
+
+              <p className="text-sm mt-1">{product.category}</p>
+            </div>
+
+            <div className="flex items-center justify-between p-2">
+              <p className="font-bold">{product.price}$</p>
+
+              <Button variant="outline" onClick={() => toggleFavorite(product)}>
+                {isFavorite ? <FaStar /> : <FaRegStar />}
+              </Button>
+            </div>
           </div>
-          <div className="flex items-center justify-between p-2">
-            <p className="font-bold">{product.price}$</p>
-            <Button
-              className={""}
-              variant="outline"
-              onClick={() => toggleFavorite(product)}
-            >
-              <FaRegStar />
-            </Button>
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
