f4515312a343247e41f69c0c449ad408577ea4f5
DenisDK <unworkdk@gmail.com>
2026-09-24 19:09:29 +0300

    middleware persist

--- modules/wishlist/store/wishlist.store.ts
+++ modules/wishlist/store/wishlist.store.ts
@@ -1,27 +1,35 @@
 import { create } from "zustand";
-import { WishlistStore } from "../types/wishlist.types";
+import { persist } from "zustand/middleware";
+import type { WishlistStore } from "../types/wishlist.types";
 
-export const useWishlistStore = create<WishlistStore>((set) => ({
-  favorites: [],
+export const useWishlistStore = create<WishlistStore>()(
+  persist(
+    (set) => ({
+      favorites: [],
 
-  toggleFavorite: (product) =>
-    set((state) => {
-      const isFavorite = state.favorites.some(
-        (favorite) => favorite.id === product.id,
-      );
+      toggleFavorite: (product) =>
+        set((state) => {
+          const isFavorite = state.favorites.some(
+            (favorite) => favorite.id === product.id,
+          );
 
-      if (isFavorite) {
-        return {
-          favorites: state.favorites.filter(
-            (favorite) => favorite.id !== product.id,
-          ),
-        };
-      }
+          if (isFavorite) {
+            return {
+              favorites: state.favorites.filter(
+                (favorite) => favorite.id !== product.id,
+              ),
+            };
+          }
 
-      return {
-        favorites: [...state.favorites, product],
-      };
+          return {
+            favorites: [...state.favorites, product],
+          };
+        }),
+
+      clearFavorites: () => set({ favorites: [] }),
     }),
-
-  clearFavorites: () => set({ favorites: [] }),
-}));
+    {
+      name: "wishlist-storage",
+    },
+  ),
+);
