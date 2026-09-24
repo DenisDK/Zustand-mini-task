868fc641e59a74c9ef99b573e4549e463bdcf663
DenisDK <unworkdk@gmail.com>
2026-09-24 18:17:39 +0300

    isEmpty wishlist

--- modules/wishlist/components/FavoritesList.tsx
+++ modules/wishlist/components/FavoritesList.tsx
@@ -12,14 +12,18 @@ const FavoritesList = () => {
     <div className="mt-5 ">
       <h2 className="font-bold mb-2">Favorites:</h2>
 
-      {favorites.map((product) => (
-        <div key={product.id} className="flex items-center gap-1 ">
-          <p>{product.name}:</p>
-          <p className="flex items-center">
-            {product.price} <FaDollarSign />
-          </p>
-        </div>
-      ))}
+      {favorites.length === 0 ? (
+        <p>Your wishlist is empty {":("}</p>
+      ) : (
+        favorites.map((product) => (
+          <div key={product.id} className="flex items-center gap-1 ">
+            <p>{product.name}:</p>
+            <p className="flex items-center">
+              {product.price} <FaDollarSign />
+            </p>
+          </div>
+        ))
+      )}
     </div>
   );
 };
