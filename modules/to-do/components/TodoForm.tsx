fd6d80e6ccc1af1b937a654af60dd51e5ddbe56e
DenisDK <unworkdk@gmail.com>
2026-09-26 20:26:45 +0300

    add todo creation via input and textarea

--- modules/to-do/components/TodoForm.tsx
+++ modules/to-do/components/TodoForm.tsx
@@ -2,22 +2,41 @@
 
 import { Button } from "@/components/ui/button";
 import { useTodoStore } from "../store/todo.store";
+import { useState } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
 
 const TodoForm = () => {
   const addTodo = useTodoStore((state) => state.addTodo);
 
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   const handleAddTodo = () => {
     addTodo({
       id: Date.now(),
-      title: "Learn Zustand",
-      description: "Practice creating Todo items with Zustand",
+      title: title,
+      description: description,
       completed: false,
     });
   };
 
   return (
     <div>
-      <Button variant="outline" className="" onClick={handleAddTodo}>
+      <div className="flex flex-col gap-1.5">
+        <Input
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          placeholder="Todo title"
+        />
+
+        <Textarea
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+          placeholder="Todo description"
+        />
+      </div>
+      <Button variant="outline" onClick={handleAddTodo} className="mt-3">
         ToDo
       </Button>
     </div>
